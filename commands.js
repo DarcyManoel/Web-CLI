6f475cf186e7fa13113bb4ffe70324e30ef2ac65
DarcyManoel <darcman00@gmail.com>
2024-10-05 19:42:18 +0930

    Create upload/download commands and structure

--- commands.js
+++ commands.js
@@ -1,3 +1,4 @@
+let data={}
 const COMMANDS={
 	root:{}}
 let commandsDirectory=`root`
@@ -10,3 +11,37 @@ function commandHelp(){
 	}
 	contentQueue+=`</table></p>`
 	terminalUpdate(contentQueue)}
+COMMANDS.root.upload=`Upload a memory file for data use.`
+function commandUpload(){
+	terminalUpdate(`<p class="feedback">File upload initiated...</p>`)
+	const fileInput=Object.assign(document.createElement(`input`),{
+		type:`file`,
+		accept:`.json`,
+		style:`display:none`,
+		onchange:(e)=>{
+			const reader=new FileReader()
+			reader.onload=(e)=>{
+				data=JSON.parse(e.target.result)
+				console.log(data)
+				terminalUpdate(`<p class="feedback">File upload successful.</p>`)}
+			reader.readAsText(e.target.files[0])
+			document.body.removeChild(fileInput)},
+		oncancel:(e)=>{
+			terminalUpdate(`<p class="feedback">File upload aborted, user action.</p>`)
+			document.body.removeChild(fileInput)}})
+	document.body.appendChild(fileInput)
+	fileInput.click()}
+COMMANDS.root.download=`Download a memory file for data storage.`
+function commandDownload(){
+	if(!Object.keys(data).length){
+		terminalUpdate(`<p class="feedback">File download aborted, no data.</p>`)
+		return}
+	const file=new Blob(
+		[JSON.stringify(data)],
+		{type:`application/json`})
+	const link=Object.assign(document.createElement(`a`),{
+		href:URL.createObjectURL(file),
+		download:`Life CLI.json`})
+	link.click()
+	URL.revokeObjectURL(link.href)
+	terminalUpdate(`<p class="feedback">File download initialised...</p>`)}
