3634ba28100e5b6a15f9da53cab9c4d9bf69ad74
DarcyManoel <darcman00@gmail.com>
2024-10-05 16:10:55 +0930

    Render commands in terminal as intractable, triggering the relevant command

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ COMMANDS.help=`Lists all available commands at the active directory.`
 function commandHelp(){
 	let contentQueue=`<p><table class="feedback">`
 	for(const[key,value]of Object.entries(COMMANDS)){
-		contentQueue+=`<tr><td class="command">${key}</td><td>${value}</td></tr>`
+		contentQueue+=`<tr><td class="command" onclick="runCommandFromKey(1,this.innerHTML)">${key}</td><td>${value}</td></tr>`
 	}
 	contentQueue+=`</table></p>`
 	TERMINAL.innerHTML+=contentQueue}
@@ -14,13 +14,19 @@ function submitCommand(value){
 	INPUT.value=``
 	INPUT_REFLECTION.innerHTML=``
 	TERMINAL.innerHTML+=`<p>user: ${value}`
-	const FEEDBACK_LOST=`<p><span class="feedback-lost">Command not found. For a list of commands, type <span class="command">'help'</span>.</p>`
+	const FEEDBACK_LOST=`<p><span class="feedback-lost">Command not found. For a list of commands, type <span class="command" onclick="runCommandFromKey(1,this.innerHTML)">'help'</span>.</p>`
 	COMMANDS[value]
-		?window[`command${capitalizeFirstLetter(value)}`]
-			?window[`command${capitalizeFirstLetter(value)}`]()
+		?runCommandFromKey(0,value)
+			?runCommandFromKey(1,value)
 			:TERMINAL.innerHTML+=FEEDBACK_LOST
 		:TERMINAL.innerHTML+=FEEDBACK_LOST
-	console.log(`command${capitalizeFirstLetter(value)}`)
 	document.body.scrollTop=document.body.scrollHeight}
 function capitalizeFirstLetter(string){
 	return string.charAt(0).toUpperCase()+string.slice(1)}
+function runCommandFromKey(doRun,key){
+	let doesFunctionExist=0
+	const COMMAND=window[`command${capitalizeFirstLetter(key.replace(/'/g,``))}`]
+	doRun
+		?COMMAND()
+		:doesFunctionExist=COMMAND
+	return doesFunctionExist}
