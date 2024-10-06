5bb88730d5ac088bffba3165d9d543a17f8a772e
DarcyManoel <darcman00@gmail.com>
2024-10-07 01:53:01 +1030

    Display command suggestion for nearest match

--- scripts.js
+++ scripts.js
@@ -1,16 +1,29 @@
 let data={}
 const INPUT=document.getElementById(`input`)
 const INPUT_REFLECTION=document.getElementById(`input-reflection`)
+const INPUT_SUGGESTION=document.getElementById(`input-suggestion`)
 const TERMINAL=document.getElementById(`terminal`)
 terminalUpdate(0,`<p class="feedback-landing">Welcome to this interactive web terminal.</p>`)
 //	input
 function inputCommand(key){
 	INPUT_REFLECTION.innerHTML=key
 	window.scrollTo(0,document.body.scrollHeight)
+	if(key.length){
+		for(let i1=0;i1<COMMANDS_SUGGESTIONS.length;i1++){
+			if(COMMANDS_SUGGESTIONS[i1].startsWith(key)){
+				INPUT_SUGGESTION.innerHTML=COMMANDS_SUGGESTIONS[i1]
+				return
+			}
+		}
+		INPUT_SUGGESTION.innerHTML=``
+	}else{
+		INPUT_SUGGESTION.innerHTML=``
+	}
 }
 function submitCommand(key){
 	INPUT.value=``
 	INPUT_REFLECTION.innerHTML=``
+	INPUT_SUGGESTION.innerHTML=``
 	document.getElementById(`command-prefix`).innerHTML=`user:`
 	terminalUpdate(1,key)
 	const FEEDBACK_LOST=`<p><span class="feedback-lost">Command not found.</p>`
@@ -55,7 +68,8 @@ COMMANDS.data={
 				reader.onload=(e)=>{
 					data=JSON.parse(e.target.result)
 					console.log(data)
-					terminalUpdate(0,`<p class="feedback">File upload successful.</p>`)}
+					terminalUpdate(0,`<p class="feedback">File upload successful.</p>`)
+				}
 				reader.readAsText(e.target.files[0])
 				document.body.removeChild(fileInput)
 			},
@@ -70,15 +84,24 @@ COMMANDS.data={
 	download:function(){
 		if(!Object.keys(data).length){
 			terminalUpdate(0,`<p class="feedback">File download aborted, no data.</p>`)
-			return}
+			return
+		}
 		const file=new Blob(
 			[JSON.stringify(data)],
-			{type:`application/json`})
+			{type:`application/json`}
+		)
 		const link=Object.assign(document.createElement(`a`),{
 			href:URL.createObjectURL(file),
-			download:`Life CLI.json`})
+			download:`Life CLI.json`
+		})
 		link.click()
 		URL.revokeObjectURL(link.href)
 		terminalUpdate(0,`<p class="feedback">File download initialised...</p>`)
+	}
+}
+const COMMANDS_SUGGESTIONS=[]
+for(const[key1,value1]of Object.entries(COMMANDS)){
+	for(const[key2,value2]of Object.entries(value1)){
+		COMMANDS_SUGGESTIONS.push(`${key1} ${key2}`)
 	}
 }
