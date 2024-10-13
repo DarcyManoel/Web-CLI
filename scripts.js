a2d39ea14c0ec76056dcdba39d4c458c558f112a
DarcyManoel <darcman00@gmail.com>
2024-10-13 17:02:00 +1030

    Feature: funds.table() will render an overview table of funds details

--- scripts.js
+++ scripts.js
@@ -49,17 +49,26 @@ function submitCommand(key){
 	INPUT_REFLECTION.innerHTML=``
 	INPUT_SUGGESTION.innerHTML=``
 	let keys=key.replace(/\)/,``).split(/[(,\s]/)
-	let command=keys[0]
+	let command=keys[0].split(`.`)
 	let arguments=keys.splice(1,keys.length)
-	TERMINAL_BACKLOG_COMMAND.push(`<span class="subtle-element">root:</span> ${command}<span class="command-arguments">(<span class="subtle-element">${arguments.join(`</span>,<span class="subtle-element">`)}</span>)</span>`)
-	try{COMMANDS[command](...arguments)}
-	catch{TERMINAL_BACKLOG_FEEDBACK.push(FEEDBACK_LOST)}
+	TERMINAL_BACKLOG_COMMAND.push(`<span class="subtle-element">root:</span> ${command.join(`.`)}<span class="command-arguments">(<span class="subtle-element">${arguments.join(`</span>,<span class="subtle-element">`)}</span>)</span>`)
+	try{
+		COMMANDS[command[0]](...arguments)
+	}
+	catch{
+		try{
+			COMMANDS[command[0]][command[1]](...arguments)
+		}
+		catch{
+			TERMINAL_BACKLOG_FEEDBACK.push(`<p>${FEEDBACK_LOST}`)
+		}
+	}
 }
 //	output
 const TERMINAL=document.getElementById(`terminal`)
 const TERMINAL_BACKLOG_COMMAND=[]
 const TERMINAL_BACKLOG_FEEDBACK=[]
-TERMINAL_BACKLOG_FEEDBACK.push(`<span class="feedback-landing">Welcome to this interactive web terminal.`)
+TERMINAL_BACKLOG_FEEDBACK.push(`<p><span class="feedback-landing">Welcome to this interactive web terminal.`)
 setInterval(terminalUpdate,100,TERMINAL_BACKLOG_COMMAND,TERMINAL_BACKLOG_FEEDBACK)
 function terminalUpdate(command,feedback){
 	if(command.length){
@@ -68,7 +77,13 @@ function terminalUpdate(command,feedback){
 		window.scrollTo(0,document.body.scrollHeight)
 	}
 	if(feedback.length){
-		TERMINAL.insertAdjacentHTML(`beforeend`,`<p>${feedback[0]}`)
+		if(typeof feedback[0]==`object`){
+			const TAGGED_ELEMENTS=document.getElementsByTagName(feedback[0].tag)
+			TAGGED_ELEMENTS[TAGGED_ELEMENTS.length-1].insertAdjacentHTML(`beforeend`,`${feedback[0].insert}`)
+			feedback.shift()
+			return
+		}
+		TERMINAL.insertAdjacentHTML(`beforeend`,`${feedback[0]}`)
 		feedback.shift()
 		window.scrollTo(0,document.body.scrollHeight)
 	}
@@ -76,7 +91,7 @@ function terminalUpdate(command,feedback){
 //	commands
 const COMMANDS={}
 COMMANDS.upload=function(){
-	TERMINAL_BACKLOG_FEEDBACK.push(`File upload initiated.`)
+	TERMINAL_BACKLOG_FEEDBACK.push(`<p>File upload initiated.`)
 	const fileInput=document.createElement(`input`)
 	fileInput.type=`file`
 	fileInput.accept=`.json`
@@ -99,7 +114,7 @@ COMMANDS.upload=function(){
 COMMANDS.download=function(){
 	const hasData=Object.keys(data).length
 	if(!hasData){
-		TERMINAL_BACKLOG_FEEDBACK.push(`File download blocked, no data to download.`)
+		TERMINAL_BACKLOG_FEEDBACK.push(`<p>File download blocked, no data to download.`)
 		return
 	}
 	const file=new Blob([JSON.stringify(data)],{type:`application/json`})
@@ -108,15 +123,46 @@ COMMANDS.download=function(){
 	link.download=`Web-CLI data.json`
 	link.click()
 	URL.revokeObjectURL(link.href)
-	TERMINAL_BACKLOG_FEEDBACK.push(`File download initialised.`)
+	TERMINAL_BACKLOG_FEEDBACK.push(`<p>File download initialised.`)
+}
+COMMANDS.funds={}
+COMMANDS.funds.table=function(){
+	if(!data.funds){
+		TERMINAL_BACKLOG_FEEDBACK.push(`<p>Render aborted, no funds data to render.`)
+		return
+	}
+	TERMINAL_BACKLOG_FEEDBACK.push(`<p>Rendering funds table.`)
+	TERMINAL_BACKLOG_FEEDBACK.push(`<table></table>`)
+	for(const[key1,value1]of Object.entries(data.funds)){
+		const BALANCE=value1.records[value1.records.length-1].balance
+		const BALANCE_INT_LENGTH=separateThousands(BALANCE).split(`.`)[0].length
+		const DAYS_SINCE_UPDATE=Math.round((new Date()-new Date(value1.records[value1.records.length-1].date.join(`-`)))/86400000)
+		console.log(DAYS_SINCE_UPDATE)
+		TERMINAL_BACKLOG_FEEDBACK.push({tag:`table`,insert:`
+			<tr>
+				<td>${key1}</td>
+				<td style="text-align:right;">$${separateThousands(BALANCE).padEnd(BALANCE_INT_LENGTH+3,`.00`)}</td>
+				<td style="text-align:right;">${DAYS_SINCE_UPDATE} days since update</td>
+			</tr>
+		`})
+	}
 }
 //	command auto-complete
 const COMMANDS_SUGGESTIONS=[]
 for(const[key1,value1]of Object.entries(COMMANDS)){
+	if(typeof value1==`object`){
+		for(const[key2,value2]of Object.entries(value1)){
+			COMMANDS_SUGGESTIONS.push(`${key1}.${key2}`)
+		}
+		continue
+	}
 	COMMANDS_SUGGESTIONS.push(`${key1}`)
 }
 //	tools
 function getArgumentNames(func){
 	const args=func.toString().match(/\(([^)]*)\)/)[1]
 	return args.split(`,`).map(arg=>arg.trim())
+}
+function separateThousands(int){
+	return int.toString().replace(/\B(?=(\d{3})+(?!\d))/g,`,`)
 }
