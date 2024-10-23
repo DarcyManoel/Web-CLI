0a03c46fdc931374013e260ef7a7f7a132eb3510
DarcyManoel <darcman00@gmail.com>
2024-10-23 22:53:28 +1030

    Print suggested commands after each run command

--- commands.js
+++ commands.js
@@ -14,6 +14,10 @@ commands.upload=function(){
 			data=JSON.parse(reader.result)
 			terminalBacklogFeedback.push(`<p>File upload completed.`)
 			terminalBacklogFeedback.push(`<p>Data last updated on ${formatDate(data.lastUpdated)}. <span class="subtle-element">(${getDaysSince(data.lastUpdated)})`)
+			terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+			Object.keys(data.funds).length
+				?terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.table()`)
+				:terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.new(`)
 			console.log(data)
 		}
 		reader.readAsText(e.target.files[0])
@@ -26,9 +30,10 @@ commands.upload=function(){
 	fileInput.click()
 }
 commands.download=function(){
-	const hasData=Object.keys(data).length
-	if(!hasData){
+	if(!Object.keys(data.funds).length){
 		terminalBacklogFeedback.push(`<p>File download blocked, no data to download.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">upload()`)
 		return
 	}
 	const file=new Blob([JSON.stringify(data)],{type:`application/json`})
@@ -41,74 +46,88 @@ commands.download=function(){
 }
 commands.funds={}
 commands.funds.table=function(){
-	try{
-		if(!Object.keys(data.funds).length){
-			terminalBacklogFeedback.push(`<p>Render aborted, no funds data to render.`)
-			return
+	if(!Object.keys(data.funds).length){
+		terminalBacklogFeedback.push(`<p>Render aborted, no funds data to render.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.new(`)
+		return
+	}
+	terminalBacklogFeedback.push(`<p>Rendering funds table.`)
+	terminalBacklogFeedback.push(`<table></table>`)
+	for(const[key,value]of Object.entries(data.funds)){
+		try{
+			const recordsLastKey=Object.keys(value.records).pop()
+			const recordsLastValue=value.records[recordsLastKey]
+			const balance=recordsLastValue.balance
+			terminalBacklogFeedback.push({tag:`table`,insert:`
+				<tr>
+					<td>${key}</td>
+					<td style="text-align:right;">$${separateThousands(balance.toFixed(2))}</td>
+				</tr>
+			`})
 		}
-		terminalBacklogFeedback.push(`<p>Rendering funds table.`)
-		terminalBacklogFeedback.push(`<table></table>`)
-		for(const[key,value]of Object.entries(data.funds)){
-			try{
-				const recordsLastKey=Object.keys(value.records).pop()
-				const recordsLastValue=value.records[recordsLastKey]
-				const balance=recordsLastValue.balance
-				terminalBacklogFeedback.push({tag:`table`,insert:`
-					<tr>
-						<td>${key}</td>
-						<td style="text-align:right;">$${separateThousands(balance.toFixed(2))}</td>
-					</tr>
-				`})
-			}
-			catch{
-				terminalBacklogFeedback.push({tag:`table`,insert:`
-					<tr>
-						<td>${key}</td>
-						<td style="text-align:right;">$0.00</td>
-					</tr>
-				`})
-			}
+		catch{
+			terminalBacklogFeedback.push({tag:`table`,insert:`
+				<tr>
+					<td>${key}</td>
+					<td style="text-align:right;">$0.00</td>
+				</tr>
+			`})
 		}
 	}
-	catch{
-		terminalBacklogFeedback.splice(-2,2)
-		terminalBacklogFeedback.push(`<p><span class="feedback-lost">Command failed, something went wrong.`)
-	}
+	terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+	terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.update(`)
 }
 commands.funds.update=function(account,balance,date){
 	//	account parameter checks
-	if(account===undefined){
+	if(!account){
 		if(!Object.keys(data.funds).length){
 			terminalBacklogFeedback.push(`<p>There are no accounts saved.`)
+			terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+			terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.new(`)
 			return
 		}
-		terminalBacklogFeedback.push(`<p>Which account needs to be updated?`)
+		terminalBacklogFeedback.push(`<p>No account specified.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
 		for(const account in data.funds){
-			terminalBacklogFeedback.push(`<p class="insert-command" onclick="setInput('funds.update(${account},')">'${account}'`)
+			terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.update(</span>${account}<span class="subtle-element">,</span>`)
 		}
 		return
 	}
 	if(!data.funds[account]){
 		terminalBacklogFeedback.push(`<p>Account '${account}' does not exist.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.update(</span>${account}`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.new(</span>${account}`)
 		return
 	}
 	//	balance parameter checks
 	if(!balance){
 		terminalBacklogFeedback.push(`<p>Account '${account}' cannot be updated without a balance.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.update(</span>${account}<span class="subtle-element">,</span>`)
 		return
 	}
 	if(isNaN(+balance)){
 		terminalBacklogFeedback.push(`<p>Account '${account}' cannot be updated with balance '${balance}'.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.update(</span>${account}<span class="subtle-element">,</span>`)
 		return
 	}
 	//	date parameter checks
 	if(date){
+		let suggestedCommands=function(){
+			terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+			terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.update(</span>${account}<span class="subtle-element">,</span>${balance}<span class="subtle-element">,</span>`)
+		}
 		if(!/^\d{4}-\d{2}-\d{2}$/.test(date)){
 			terminalBacklogFeedback.push(`<p>Date format is invalid.`)
+			suggestedCommands()
 			return
 		}
 		if(new Date(date)==`Invalid Date`){
 			terminalBacklogFeedback.push(`<p>Date value is invalid.`)
+			suggestedCommands()
 			return
 		}
 	}
@@ -117,19 +136,24 @@ commands.funds.update=function(account,balance,date){
 	data.funds[account].records[date||getDateToday()]={
 		balance
 	}
-	terminalBacklogFeedback.push(`<p>Account <span onclick="setInput('funds.update(${account},')">'${account}'</span></span> updated on date: ${date||getDateToday()} with balance: $${balance.toFixed(2)}`)
 	data.lastUpdated=getDateToday()
+	terminalBacklogFeedback.push(`<p>Account <span onclick="setInput('funds.update(${account},')">'${account}'</span></span> updated on date: ${date||getDateToday()} with balance: $${balance.toFixed(2)}`)
+	terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+	terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.table()`)
 }
-commands.funds.new=function(name){
-	if(name==undefined){
-		terminalBacklogFeedback.push(`<p>Cannot create account with no given name. <span class="insert-command" onclick="setInput('funds.new(')">Try again?`)
+commands.funds.new=function(account){
+	if(account==undefined){
+		terminalBacklogFeedback.push(`<p>Cannot create account with no given name.`)
+		terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+		terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)">funds.new(`)
 		return
 	}
-	data.funds[name]={
+	data.funds[account]={
 		records:{}
 	}
-	terminalBacklogFeedback.push(`<p>New account '${name}' created.`)
-	terminalBacklogFeedback.push(`<p>Update account <span class="insert-command" onclick="setInput('funds.update(${name},')">'${name}'</span> with an initial balance?`)
+	terminalBacklogFeedback.push(`<p>New account '${account}' created.`)
+	terminalBacklogFeedback.push(`<p class="suggested-commands-opener">Suggested commands:`)
+	terminalBacklogFeedback.push(`<p onclick="setInput(this.innerText)"><span class="subtle-element">funds.update(</span>${account}<span class="subtle-element">,</span>`)
 	data.lastUpdated=getDateToday()
 }
 //	command auto-complete
